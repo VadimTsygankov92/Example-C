1e81eb44a3108cb4c70ec63f8e8f860bc20c8735
Vadim Tsygankov <v-tsygankov92@mail.ru>
2023-03-22 22:31:20 +0300

    Exampl 005 Condition If Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "вадим") 
+  {
+    Console.WriteLine("Урааа, это же ВАДИМ!!!");
+  }
+  else
+  {
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+  }
